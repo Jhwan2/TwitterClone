e36a82ccbfab98440b57c26273032bed229e7e74
‘Jhwan2’ <ehgp77@naver.com>
2023-02-23 23:26:20 +0900

    24-3

--- TwitterCloneApp/Controller/TweetController.swift
+++ TwitterCloneApp/Controller/TweetController.swift
@@ -48,7 +48,7 @@ class TweetContoller: UICollectionViewController {
         }
     }
     
-    
+
     //MARK: Helpers
     func configureCollectionView() {
         collectionView.backgroundColor = .white
